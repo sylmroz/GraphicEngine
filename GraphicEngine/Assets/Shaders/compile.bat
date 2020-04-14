16a5b6b691c60a49ea89dd86adbba8ddda3e0737
K0ntre201 <sylwinho555@wp.pl>
2020-04-14 13:14:50 +0200

    Add visual studio solution files to easier migrate project

---
+++ GraphicEngine/Assets/Shaders/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.1.130.0\Bin32\glslc.exe ./glsl/basicPCVP.vert -o ./spv\basicPCVP.vert.spv
+C:\VulkanSDK\1.1.130.0\Bin32\glslc.exe ./glsl/basicPCVP.frag -o ./spv\basicPCVP.frag.spv
+pause
