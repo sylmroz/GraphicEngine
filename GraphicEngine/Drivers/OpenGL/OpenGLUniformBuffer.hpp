bbd561d37a1bc99a202eab43256e54065581e54f
K0ntre201 <sylwinho555@wp.pl>
2020-05-12 18:20:23 +0200

    Split OpenGL driver into more files

--- GraphicEngine/Drivers/OpenGL/OpenGLUniformBuffer.hpp
+++ GraphicEngine/Drivers/OpenGL/OpenGLUniformBuffer.hpp
@@ -8,40 +8,6 @@
 
 namespace GraphicEngine::OpenGL
 {
-	template <typename Uniform, typename T>
-	class UniformData
-	{
-	public:
-		UniformData(std::shared_ptr<OpenGLShaderProgram> openGlShaderProgram, const std::string& name) :
-			m_openGlShaderProgram(openGlShaderProgram),
-			m_name(name)
-		{}
-
-		void update(T val)
-		{
-			static_cast<Uniform const&>(*this).update(val);
-		}
-		
-	protected:
-		std::shared_ptr<OpenGLShaderProgram> m_openGlShaderProgram;
-		std::string m_name;
-	};
-
-
-	class UniformBufferMatrix : public UniformData<UniformBufferMatrix, glm::mat4>
-	{
-	public:
-		UniformBufferMatrix(std::shared_ptr<OpenGLShaderProgram> openGlShaderProgram, const std::string& name) :
-			UniformData<UniformBufferMatrix, glm::mat4>(openGlShaderProgram, name)
-		{}
-
-		void update(const glm::mat4& val)
-		{
-			auto uniformLocation = glGetUniformLocation(m_openGlShaderProgram->getShaderProgramId(), m_name.c_str());
-			glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(val));
-		}
-	};
-
 	template <typename T>
 	class UniformBuffer
 	{
