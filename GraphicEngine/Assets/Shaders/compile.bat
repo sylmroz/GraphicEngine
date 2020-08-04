d3776ae9ca5bfd30b8199ebbd57060b0d08dc1fb
K0ntre201 <sylwinho555@wp.pl>
2020-08-04 19:40:15 +0200

    Modify compiler file to pass args

--- GraphicEngine/Assets/Shaders/compile.bat
+++ GraphicEngine/Assets/Shaders/compile.bat
@@ -1,3 +1,16 @@
-C:\VulkanSDK\1.1.130.0\Bin32\glslc.exe ./glsl/basicPCTVP.vert -o ./spv\basicPCTVP.vert.spv
-C:\VulkanSDK\1.1.130.0\Bin32\glslc.exe ./glsl/basicPCTVP.frag -o ./spv\basicPCTVP.frag.spv
-pause
+
+@echo off
+setlocal enabledelayedexpansion
+set arg1=%1
+set argCount=0
+for %%x in (%*) do (
+   set /A argCount+=1
+   set "argVec[!argCount!]=%%~x"
+)
+
+echo Number of processed arguments: %argCount%
+
+for /L %%i in (2,1,%argCount%) do (
+    echo %%i- "!argVec[%%i]!"
+    C:\VulkanSDK\1.1.130.0\Bin32\glslc.exe ./glsl/%arg1%."!argVec[%%i]!" -o ./spv/%arg1%."!argVec[%%i]!".spv
+)
