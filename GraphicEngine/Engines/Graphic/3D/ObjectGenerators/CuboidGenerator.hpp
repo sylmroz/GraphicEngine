8da6a39df2acf5e76065c5bf98f6317ed2fd8179
K0ntre201 <sylwinho555@wp.pl>
2020-10-18 21:13:02 +0200

    Correct calculation if indexes for two {x,z} and two {x,y} walls.

--- GraphicEngine/Engines/Graphic/3D/ObjectGenerators/CuboidGenerator.hpp
+++ GraphicEngine/Engines/Graphic/3D/ObjectGenerators/CuboidGenerator.hpp
@@ -35,6 +35,8 @@ namespace GraphicEngine::Engines::Graphic
 			step.y /= scale.y;
 			step.z /= scale.z;
 
+			// Vertices
+
 			// first wall x,z
 			for (uint32_t z{ 0 }; z < scale.z + 1; ++z)
 			{
@@ -101,6 +103,177 @@ namespace GraphicEngine::Engines::Graphic
 					v->position = glm::vec3(from + step * glm::vec3(0.0f, y, z));
 					v->position.x = to.x;
 					vertices.push_back(v);
+				}
+			}
+
+			// Faces
+
+			// first wall x,z
+			for (uint32_t z{ 0 }; z < scale.z; ++z)
+			{
+				for (uint32_t x{ 0 }; x < scale.x; ++x)
+				{
+					uint32_t point = x + (z * (scale.x + 1));
+					if (triangleDirection == TriangleDirection::CounterClockwise)
+					{
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1 + scale.x, point + 2 + scale.x })));
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 })));
+					}
+
+					else
+					{
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1, point + 2 + scale.x })));
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 + scale.x })));
+					}
+				}
+			}
+
+			// second wall x,z
+			for (uint32_t z{ 0 }; z < scale.z; ++z)
+			{
+				auto offset = (scale.x + 1) * (scale.z + 1);
+				for (uint32_t x{ 0 }; x < scale.x; ++x)
+				{
+					uint32_t point = x + (z * (scale.x + 1)) + offset;
+					if (triangleDirection == TriangleDirection::CounterClockwise)
+					{
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1, point + 2 + scale.x })));
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 + scale.x })));
+					}
+
+					else
+					{
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1 + scale.x, point + 2 + scale.x })));
+						faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 })));
+					}
+				}
+			}
+
+			// first wall x,y
+			for (uint32_t y{ 0 }; y < scale.y; ++y)
+			{
+				auto offset = 2 * (scale.x + 1) * (scale.z + 1);
+				for (uint32_t x{ 0 }; x < scale.x; ++x)
+				{
+					uint32_t point = x + (y * (scale.x + 1)) + offset;
+					if (triangleDirection == TriangleDirection::CounterClockwise)
+					{
+						if (y == 0)
+						{
+							uint32_t boundary = point - (scale.x + 1) * (scale.z + 1);
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ boundary,  point + 1, point })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ boundary, boundary + 1, point + 1 })));
+						}
+
+						else if (y == scale.y - 1)
+						{
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, x + 1, x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1, x + 1 })));
+
+						}
+
+						else
+						{
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 + scale.x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1, point + 2 + scale.x })));
+						}
+					}
+
+					else
+					{
+						if (y == 0)
+						{
+							uint32_t boundary = point - (scale.x + 1) * (scale.z + 1);
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ boundary, point, point + 1 })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ boundary, point + 1, boundary + 1 })));
+						}
+
+						else if (y == scale.y - 1)
+						{
+							point -= (scale.x + 1);
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, x, x + 1 })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, x + 1, point + 1 })));
+						}
+
+						else
+						{
+							point -= (scale.x + 1);
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1 + scale.x, point + 2 + scale.x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 })));
+						}
+					}
+				}
+			}
+
+			// second wall x,y
+			for (uint32_t y{ 0 }; y < scale.y; ++y)
+			{
+				auto baseOffset = (2 * (scale.x + 1) * (scale.z + 1)) + (scale.x + 1) * (scale.y - 1);
+				for (uint32_t x{ 0 }; x < scale.x; ++x)
+				{
+					uint32_t point = x + (y * (scale.x + 1)) + baseOffset;
+					if (triangleDirection == TriangleDirection::CounterClockwise)
+					{
+						if (y == 0)
+						{
+							uint32_t offset = (scale.x + 1) * scale.z;
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset + x, point + 1 + scale.x, point + 2 + scale.x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset, point + 2 + scale.x, offset + 1 })));
+						}
+
+						else if (y == scale.y - 1)
+						{
+							uint32_t offset = (2*(scale.x + 1) * (scale.z + 1)) - scale.z;
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset + x, point + 1, point + 2 })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset, point + 2, offset + 1 })));
+						}
+
+						else
+						{
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1 + scale.x, point + 2 + scale.x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 })));
+						}
+					}
+
+					else
+					{
+						if (y == 0)
+						{
+							uint32_t offset = (scale.x + 1) * scale.z;
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset + x, point, point + 1 })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ offset + x, point + 1, offset + x + 1 })));
+						}
+
+						else if (y == scale.y - 1)
+						{
+							point -= (scale.x + 1);
+							uint32_t offset = (2 * (scale.x + 1) * (scale.z + 1)) - (scale.x + 1);
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, offset + x + 1, point + 1 })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, offset + x, offset + x + 1 })));
+						}
+
+						else
+						{
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 1, point + 2 + scale.x })));
+							faces.push_back(std::make_shared<Scene::Face>(Scene::Face({ point, point + 2 + scale.x, point + 1 + scale.x })));
+						}
+					}
+				}
+			}
+
+			// first wall y,z
+			for (uint32_t z{ 0 }; z < scale.z; ++z)
+			{
+				for (uint32_t y{ 0 }; y < scale.y; ++y)
+				{
+				}
+			}
+
+			// second wall y,z
+			for (uint32_t z{ 0 }; z < scale.z; ++z)
+			{
+				for (uint32_t y{ 0 }; y < scale.y; ++y)
+				{
 				}
 			}
 
--- GraphicEngineTests/ObjectGenerators.cpp
+++ GraphicEngineTests/ObjectGenerators.cpp
@@ -8,7 +8,7 @@ using namespace GraphicEngine::Common;
 
 TEST(Cuboid_Generator, Check_size_of_generated_vector)
 {
-	auto scale = glm::ivec3(5, 20, 30);
+	auto scale = glm::ivec3(2, 2, 3);
 	auto verticesSize = 2 * (((scale.x + 1) * (scale.z + 1)) + ((scale.x + 1) * (scale.y - 1)) + ((scale.y - 1) * (scale.z - 1)));
 	auto [vertices, faces, boudingBox] = CuboidGenerator<VertexPN>{}.getObject(glm::vec3(-10.0f), glm::vec3(10.0f), scale);
 	EXPECT_EQ(vertices.size(), verticesSize);
